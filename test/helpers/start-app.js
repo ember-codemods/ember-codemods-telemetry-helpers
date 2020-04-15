343cdc01a9eed80eb5e26125ac615e6f0dbee16c
Suchita Doshi <sudoshi@linkedin.com>
2020-04-15 09:09:29 -0700

    use ember serve instead of yarn start

--- test/helpers/start-app.js
+++ test/helpers/start-app.js
@@ -3,14 +3,14 @@ const path = require('path');
 
 module.exports = async function startApp(appPath) {
   const classicAppDir = path.resolve(appPath);
-  const execOpts = { cwd: classicAppDir, stderr: 'inherit' };
+  const execOpts = { cwd: classicAppDir, stderr: 'inherit', preferLocal: true };
   console.log('installing deps');
 
   await execa('rm', ['-rf', 'node_modules'], execOpts);
   await execa('yarn', ['install'], execOpts);
 
   console.log('starting serve');
-  const emberServe = execa('yarn', ['start'], execOpts);
+  const emberServe = execa('ember', ['serve'], execOpts);
   emberServe.stdout.pipe(process.stdout);
 
   await new Promise(resolve => {
@@ -21,6 +21,18 @@ module.exports = async function startApp(appPath) {
       }
     });
   });
+
+  emberServe.shutdown = async function() {
+    this.kill();
+    try {
+      await this;
+    } catch (e) {
+      // Process is allowed to exit with a non zero exit status code.
+    }
+    if (!this.killed) {
+      throw new Error(`the process ${this.pid} wasn't killed.`);
+    }
+  };
 
   return { emberServe };
 };
